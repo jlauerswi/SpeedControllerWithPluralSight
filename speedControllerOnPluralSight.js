2c8989d792c842e4674416dbc69c1bf7b355a7bc
jlauerswi <41702008+jlauerswi@users.noreply.github.com>
2019-10-21 10:44:28 -0400

    Fixed scoping.

--- speedControllerOnPluralSight.js
+++ speedControllerOnPluralSight.js
@@ -8,33 +8,34 @@
             clearInterval(checkExistsDiv);
         }
      }, 100);
+     
+     function createDivObserver() {
+         var dom_observer = new MutationObserver(function(mutation) {
+             var speedControllerParent = document.querySelector(".vsc-controller");
+             if (speedControllerParent) {
+                 attachSpeedSliderToVideoPlayerButton();
+             }
+         });
+         var container = document.querySelector("div[interaction-mode] > div");
+         var config = { attributes: true, childList: true, characterData: true };
+         dom_observer.observe(container, config);
+     }
+     
+     function attachSpeedSliderToVideoPlayerButton() {
+         var speedController = document.querySelector(".vsc-controller").shadowRoot.querySelector("span.draggable");
+         var observer = new MutationObserver(function(e) {
+             var num = e[0].target.innerText;
+             // Matches range on PluralSight's speed slider
+             if (num >= 0.5 && num <= 2.0) {
+                 document.querySelector("button[aria-label='" + Number(num)+ "']").click();
+             }
+         });
+     
+         observer.observe(speedController, {
+             characterData: true,
+             childList: true
+         });
+     }
 
 })();
 
-function createDivObserver() {
-    var dom_observer = new MutationObserver(function(mutation) {
-        var speedControllerParent = document.querySelector(".vsc-controller");
-        if (speedControllerParent) {
-            attachSpeedSliderToVideoPlayerButton();
-        }
-    });
-    var container = document.querySelector("div[interaction-mode] > div");
-    var config = { attributes: true, childList: true, characterData: true };
-    dom_observer.observe(container, config);
-}
-
-function attachSpeedSliderToVideoPlayerButton() {
-    var speedController = document.querySelector(".vsc-controller").shadowRoot.querySelector("span.draggable");
-    var observer = new MutationObserver(function(e) {
-        var num = e[0].target.innerText;
-        // Matches range on PluralSight's speed slider
-        if (num >= 0.5 && num <= 2.0) {
-            document.querySelector("button[aria-label='" + Number(num)+ "']").click();
-        }
-    });
-
-    observer.observe(speedController, {
-        characterData: true,
-        childList: true
-    });
-}
